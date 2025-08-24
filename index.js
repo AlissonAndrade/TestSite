4bfee6587ca734aa83ddd0427a96d96d8dd6279a
Alisson Andrade <136262192+AlissonAndrade@users.noreply.github.com>
2025-08-24 01:17:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const abrir = document.getElementById("abrirnav");
+const content = document.getElementById("divnav");
+const fechar = document.getElementById("closenav");
+
+abrir.addEventListener('click', ()=>{
+    content.style.display = "block",
+    abrir.style.visibility = "hidden";
+});
+
+fechar.addEventListener('click', ()=>{
+    content.style.display = "none",
+    abrir.style.visibility = "inherit";
+});
