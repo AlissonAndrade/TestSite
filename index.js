932710e00988304471c970f3b4cf227e1475f173
Alisson Andrade <136262192+AlissonAndrade@users.noreply.github.com>
2025-09-01 21:56:36 -0300

    pequenas mudanças de estilos
    melhorias no footer e no menu

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fechar = document.getElementById("closenav");
 abrir.addEventListener('click', ()=>{
     content.style.display = "block",
     abrir.style.visibility = "hidden";
+     
 });
 
 fechar.addEventListener('click', ()=>{
